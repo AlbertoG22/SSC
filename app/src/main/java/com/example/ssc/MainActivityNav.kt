549ab871067ee453545d29ec38d22b228ffe902c
José Alberto Negrete González <jnegrete15@ucol.mx>
2020-08-20 12:52:13 -0500

    Login implementado y funcionando

--- app/src/main/java/com/example/ssc/MainActivityNav.kt
+++ app/src/main/java/com/example/ssc/MainActivityNav.kt
@@ -1,5 +1,6 @@
 package com.example.ssc
 
+import android.content.Context
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -7,12 +8,36 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.facebook.login.LoginManager
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main_nav.*
+import kotlinx.android.synthetic.main.fragment_home.*
+
+enum class ProviderType{
+    BASIC,
+    GOOGLE,
+    FACEBOOK
+}
 
 class MainActivityNav : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_nav)
+
+        //(2)
+        val bundle:Bundle? = intent.extras
+        val email:String? = bundle?.getString("email") //el "email" es el parámetro que declaramos
+        val provider:String? = bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+
+        //Guardado de datos
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email) //email que llega a HomeActivity
+        prefs.putString("provider", provider)
+        prefs.apply()
+        //
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
@@ -23,4 +48,27 @@ class MainActivityNav : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
+    //(3)
+    private fun setup(email:String, provider:String){
+        title = "Inicio"
+        //emailTextView.text = email
+        //providerTextView.text = provider
+
+        logOutBtn.setOnClickListener {
+
+            //Borrado de datos
+            val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
+            if(provider == ProviderType.FACEBOOK.name){
+                LoginManager.getInstance().logOut()
+            }
+
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+    }
+    //
 }
--- app/src/main/java/com/example/ssc/ui/HomeFragment/HomeFragment.kt
+++ app/src/main/java/com/example/ssc/ui/HomeFragment/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.ssc.ui.HomeFragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +9,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.ssc.ProviderType
 import com.example.ssc.R
+import com.facebook.login.LoginManager
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_home.*
+
 
 class HomeFragment : Fragment() {
 
@@ -19,6 +25,24 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        //(2)
+        /*
+        val bundle:Bundle? = intent.extras
+        val email:String? = bundle?.getString("email") //el "email" es el parámetro que declaramos
+        val provider:String? = bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+
+        //Guardado de datos
+        val prefs =
+            activity?.getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)?.edit()
+        //val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs?.putString("email", email) //email que llega a HomeActivity
+        prefs?.putString("provider", provider)
+        prefs?.apply()
+         */
+        //
+
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
@@ -28,4 +52,31 @@ class HomeFragment : Fragment() {
         })
         return root
     }
+
+    //(3)
+    /*
+    private fun setup(email:String, provider:String){
+        //title = "Inicio"
+        //emailTextView.text = email
+        //providerTextView.text = provider
+
+        logOutBtn.setOnClickListener {
+
+            //Borrado de datos
+            val prefs =
+                activity?.getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)?.edit()
+            //val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs?.clear()
+            prefs?.apply()
+
+            if(provider == ProviderType.FACEBOOK.name){
+                LoginManager.getInstance().logOut()
+            }
+
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+    }
+     */
+    //
 }
